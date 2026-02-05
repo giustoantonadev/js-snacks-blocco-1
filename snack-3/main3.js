685fbfafecab8900c648a4250831d2a1badddc41
giustoantonadev <giustoantonadev@gmail.com>
2026-02-05 12:12:34 +0100

    let's go

---
+++ snack-3/main3.js
@@ -0,0 +1,14 @@
+console.log('it works');
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+let number = 0;
+
+for (let i = 0; i < 10; i++) {
+    const numbers = Number(prompt('Type a number'));
+    number += numbers;
+}
+
+console.log(number);
